47859ec02005e7d0dd37c9e24bce135ba3ae632b
cassandreh1 <cassandreh01@gmail.com>
2020-06-07 17:57:25 -0400

    initial styling of technical skill section

--- src/app/technical-skills/technical-skills.component.ts
+++ src/app/technical-skills/technical-skills.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-technical-skills',
   templateUrl: './technical-skills.component.html',
-  styleUrls: ['./technical-skills.component.css']
+  styleUrls: ['./technical-skills.component.scss']
 })
 export class TechnicalSkillsComponent implements OnInit {
  technologies: string[] = [ 'JavaScript',
