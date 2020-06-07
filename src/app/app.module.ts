71fab5864c57911fc836cb76006d5ee8ecfa367c
cassandreh1 <cassandreh01@gmail.com>
2020-06-07 10:59:12 -0400

    updating ng version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+// import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -27,7 +27,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
+    // HttpModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
