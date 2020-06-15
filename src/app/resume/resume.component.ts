139211f0a18d0df00c05be899838144df49ab69d
cassandreh1 <cassandreh01@gmail.com>
2020-06-15 19:39:35 -0400

    adding resume pdf and stylingin of resume

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faFilePdf } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.scss']
 })
 export class ResumeComponent implements OnInit {
-  public subtitle: string;
-
+  faFilePdf = faFilePdf
   constructor() { }
-
   ngOnInit() {
   }
 
