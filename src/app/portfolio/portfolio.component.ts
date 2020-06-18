c7de74c5fc93a83323e083320e472edeccdec708
cassandreh1 <cassandreh01@gmail.com>
2020-06-18 17:20:27 -0400

    adding porfolio logos resume styling updates

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faHardHat } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-portfolio',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent implements OnInit {
+  faHardHat = faHardHat;
 
   constructor() { }
 
