b0a47ff75794c9b2be56c9c8dfe21d3d96de254f
cassandreh1 <cassandreh01@gmail.com>
2020-06-20 18:57:19 -0400

    adding about me text adding props to technical skill comp and brand icons

--- src/app/technical-skills/technical-skills.component.ts
+++ src/app/technical-skills/technical-skills.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+// import { faVuejs, faAngular, faHtml5, faJsSquare, faCss3, faSass, faNodeJs, faGit, 
+// faBitbucket, faAtom} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-technical-skills',
@@ -6,25 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./technical-skills.component.scss']
 })
 export class TechnicalSkillsComponent implements OnInit {
- technologies: string[] = [ 'JavaScript',
-  'Vue.js',
-  'Angular 5',
-  'TypeScript with ES6 features',
-  'jQuery',
-  'HTML5',
-  'CSS',
-  'SCSS',
-  'Node.js',
-  'Chai.js',
-  'Mocha.js',
-  'Jasmine.js']
+  @Input() bgColor: String;
+  @Input() titleColor: String;
+  @Input() hrColor: String;
+ technologies: string[] = [ 
+   'devicon-javascript-plain colored',
+   'devicon-vuejs-plain colored',
+   'devicon-angularjs-plain colored',
+   'devicon-typescript-plain colored',
+   'devicon-jquery-plain colored',
+   'devicon-html5-plain colored',
+   'devicon-css3-plain colored',
+   'devicon-sass-plain colored',
+   'devicon-nodejs-plain colored',
+   'devicon-jasmine-plain colored',
+]
  methodologies: string[] = ['Agile (Lean/Scrum)', 'Waterfall'];
  developmentTools: string[] =[
-  'Visual Studio Code',
-  'Atom',
-  'Brackets',
-  'GitHub/Git',
-  'BitBucket',
+   'devicon-visualstudio-plain colored',
+   'devicon-atom-original colored',
+   'devicon-github-plain colored',
+   'devicon-git-plain colored',
+   'devicon-bitbucket-plain colored',
  ];
 
   constructor() { }
