7b170939a3003ee6f30c68b0600a455ea6a99f00
cassandreh1 <cassandreh01@gmail.com>
2020-06-22 14:53:24 -0400

    adding tooltips for skils logos

--- src/app/technical-skills/technical-skills.component.ts
+++ src/app/technical-skills/technical-skills.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-// import { faVuejs, faAngular, faHtml5, faJsSquare, faCss3, faSass, faNodeJs, faGit, 
-// faBitbucket, faAtom} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-technical-skills',
@@ -11,25 +9,70 @@ export class TechnicalSkillsComponent implements OnInit {
   @Input() bgColor: String;
   @Input() titleColor: String;
   @Input() hrColor: String;
- technologies: string[] = [ 
-   'devicon-javascript-plain colored',
-   'devicon-vuejs-plain colored',
-   'devicon-angularjs-plain colored',
-   'devicon-typescript-plain colored',
-   'devicon-jquery-plain colored',
-   'devicon-html5-plain colored',
-   'devicon-css3-plain colored',
-   'devicon-sass-plain colored',
-   'devicon-nodejs-plain colored',
-   'devicon-jasmine-plain colored',
+ technologies: Object[] = [ 
+  {
+    icon: 'devicon-javascript-plain colored',
+    text:'JavaScript',
+  },
+  {
+    icon: 'devicon-vuejs-plain colored',
+    text:'Vue.js',
+  },
+  {
+    icon: 'devicon-angularjs-plain colored',
+    text:'Angular 9',
+  },
+  {
+    icon: 'devicon-typescript-plain colored',
+    text:'TypeScript.js',
+  },
+  {
+    icon: 'devicon-jquery-plain colored',
+    text:'jQuery',
+  },
+  {
+    icon: 'devicon-html5-plain colored',
+    text:'HTML5',
+  },
+  {
+    icon: 'devicon-css3-plain colored',
+    text:'CSS3',
+  },
+  {
+    icon: 'devicon-sass-plain colored',
+    text:'SCSS',
+  },
+  {
+    icon:'devicon-nodejs-plain colored',
+    text:'Node.js',
+  },
+  {
+    icon: 'devicon-jasmine-plain colored',
+    text:'Jasmine.js',
+  },
 ]
  methodologies: string[] = ['Agile (Lean/Scrum)', 'Waterfall'];
- developmentTools: string[] =[
-   'devicon-visualstudio-plain colored',
-   'devicon-atom-original colored',
-   'devicon-github-plain colored',
-   'devicon-git-plain colored',
-   'devicon-bitbucket-plain colored',
+ developmentTools: Object[] =[
+  {
+    icon: 'devicon-visualstudio-plain colored',
+    text:'Visual Studio Code',
+  },
+  {
+    icon: 'devicon-atom-original colored',
+    text:'Atom',
+  },
+  {
+    icon: 'devicon-git-plain colored',
+    text:'Git',
+  },
+  {
+    icon: 'devicon-github-plain colored',
+    text:'GitHub',
+  },
+  {
+    icon: 'devicon-bitbucket-plain colored',
+    text:'BitBucket',
+  },
  ];
 
   constructor() { }
